8c213f47b3583031372003ccb1932a9ac1071626
Ross Leitch <Ross@metamorphicsolutions.com>
2017-10-09 17:17:47 +1300

    Init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-import React, {Component} from 'react'
+'use strict';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+var PouchDB = require('pouchdb');
+
+export default class PouchConnector {
+   constructor(opts){
+      this.db = new PouchDB(opts.url, {
+         auth: {
+            username: opts.username,
+            password: opts.password
+         }
+      });
+   }
+
+   getDataById(id){
+      return this.db.get(id);
+   }
+
+   deleteDataById(id){
+      return this.db.remove(id);
+   }
+
+   saveDataById(id, data){
+      var d = Object.assign({}, {_id: id}, data);
+      return this.db.put(d);
+   }
+
+   getDataByModel(model_id){
+      return this.db.allDocs().then((data) => {
+         return data.filter((x) => {
+            if(x.type === model_id){
+               return x;
+            }
+         });
+      });
+   }
 }
